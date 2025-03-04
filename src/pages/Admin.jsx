a48d0d136e20059f14f2bbae13fbbb3aa8921415
Agos <agozavia@gmail.com>
2025-03-05 00:02:46 +0100

    fixed responsive issues with profile buttons

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,6 +3,7 @@ import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
 import CategoryManager from "../components/CategoryManager/CategoryManager";
 import FilterManager from "../components/FilterManager/FilterManager";
+import Styles from "../pages/Profile/Profile.module.css";
 
 const Admin = () => {
   const { user, loading } = useContext(UserContext);
@@ -19,7 +20,12 @@ const Admin = () => {
   return (
     <>
       <h1>Panel de administración</h1>
-      <Link to="/profile">Volver al perfil</Link>
+      <Link to="/profile" className={Styles.backButton}>
+        <svg className={Styles.arrowIcon} viewBox="0 0 24 24">
+          <polyline points="15 18 9 12 15 6" /> {/* Left arrow */}
+        </svg>
+        <h5>Volver al perfil</h5>
+      </Link>
       <CategoryManager />
       <FilterManager />
     </>
